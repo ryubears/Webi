d1307d6235e94035c9386f9ad31a796b24cbbddb
ryubears <ryubears@gmail.com>
2018-01-27 20:21:53 -0600

    Add Facebook Login with Firebase Authentication

--- app/src/main/java/com/yehyunryu/android/webi/LoginActivity.java
+++ app/src/main/java/com/yehyunryu/android/webi/LoginActivity.java
@@ -1,13 +1,118 @@
 package com.yehyunryu.android.webi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+
+    private LoginButton mLoginButton;
+
+    private CallbackManager mCallbackManager;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginButton = (LoginButton) findViewById(R.id.login_facebook);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i(LOG_TAG, "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(LOG_TAG, "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "printHashKey()", e);
+        }
+
+        mCallbackManager = CallbackManager.Factory.create();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mLoginButton.setReadPermissions("public_profile");
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(LOG_TAG, "Facebook Login: Successful");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(LOG_TAG, "Facebook Login: Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(LOG_TAG, "Facebook Login: " + error.toString());
+                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void launchMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(LOG_TAG, "Firebase Auth: signInWithCredential successful");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            launchMainActivity();
+                        } else {
+                            Log.e(LOG_TAG, "Firebase Auth: signInWithCredential failure: ", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mFirebaseAuth.getCurrentUser() != null) launchMainActivity();
     }
 }
